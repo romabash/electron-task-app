1b7cf31223b6fcc7dcaf8980d908e440b97f8984
Roma <romanofkin@gmail.com>
2017-05-03 17:04:46 -0700

    Add Electron as a dev dependency

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const {app, BrowserWindow} = electron;
 app.on('ready', () => {
   let win = new BrowserWindow({
     width: 500,
-    height:700,
-    frame: false
+    height:700
+    //frame: false
   });
   win.loadURL(`file://${__dirname}/index.html`);
 })
