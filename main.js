9afb6aabde6843325843b9be2a2cdf4348de6e3f
Roma <romanofkin@gmail.com>
2017-05-05 19:44:55 -0700

    Add Menu to main.js

--- main.js
+++ main.js
@@ -1,11 +1,88 @@
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
+
+let win;
 
 app.on('ready', () => {
-  let win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 500,
     height:700
     //frame: false
   });
   win.loadURL(`file://${__dirname}/index.html`);
+
+  win.on('closed', () => {
+    win = null
+  });
 })
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // For macOS
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+//Menu
+const template = [
+  {
+     label: 'File',
+     submenu: [
+        {
+           role: 'minimize'
+        },
+        {
+           role: 'close'
+        }
+     ]
+  },
+   {
+      label: 'Edit',
+      submenu: [
+         {
+            role: 'undo'
+         },
+         {
+            role: 'redo'
+         },
+         {
+            type: 'separator'
+         },
+         {
+            role: 'cut'
+         },
+         {
+            role: 'copy'
+         },
+         {
+            role: 'paste'
+         }
+      ]
+   },
+   {
+      label: 'View',
+      submenu: [
+         {
+            role: 'zoomin'
+         },
+         {
+            role: 'zoomout'
+         },
+         {
+            role: 'resetzoom'
+         }
+      ]
+   }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
