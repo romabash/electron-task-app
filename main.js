d8602d5d3e924c43ebd2d5a8fd26e9e6e8a26e79
Roma <romanofkin@gmail.com>
2017-05-06 13:49:23 -0700

    Add Windows icon

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ let win;
 app.on('ready', () => {
   win = new BrowserWindow({
     width: 500,
-    height:700
+    minWidth: 400,
+    height:600,
+    minHeight: 300
     //frame: false
   });
   win.loadURL(`file://${__dirname}/index.html`);
@@ -37,52 +39,42 @@ const template = [
   {
      label: 'File',
      submenu: [
-        {
-           role: 'minimize'
-        },
-        {
-           role: 'close'
-        }
+        { role: 'minimize' },
+        { role: 'close' }
      ]
   },
    {
       label: 'Edit',
       submenu: [
-         {
-            role: 'undo'
-         },
-         {
-            role: 'redo'
-         },
-         {
-            type: 'separator'
-         },
-         {
-            role: 'cut'
-         },
-         {
-            role: 'copy'
-         },
-         {
-            role: 'paste'
-         }
+         { role: 'undo' },
+         { role: 'redo' },
+         { type: 'separator' },
+         { role: 'cut' },
+         { role: 'copy' },
+         { role: 'paste' }
       ]
    },
    {
       label: 'View',
       submenu: [
-         {
-            role: 'zoomin'
-         },
-         {
-            role: 'zoomout'
-         },
-         {
-            role: 'resetzoom'
-         }
+         { role: 'zoomin' },
+         { role: 'zoomout' },
+         { role: 'resetzoom' }
       ]
    }
 ]
 
 const menu = Menu.buildFromTemplate(template)
 Menu.setApplicationMenu(menu)
+
+
+// {
+//    role: 'help',
+//    submenu: [
+//       {
+//         role: 'about',
+//         click() { console.log("This is about page");}
+//
+//       }
+//    ]
+// }
